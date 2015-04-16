676f7254bf80f147c070e2ae1327569c020a6ef9
EmilyPlatzer <emilyplatzer@gmail.com>
2015-04-16 08:34:24 -0600

    Chapter 3, exercise 1 - Math.min

---
+++ chapter3/1min.js
@@ -0,0 +1,12 @@
+// Your code here.
+function min(numA, numB) {
+  if (numA < numB)
+    return numA;
+  else
+    return numB;
+};
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
