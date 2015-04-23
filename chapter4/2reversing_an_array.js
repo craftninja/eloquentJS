7d3e9644e836a476e2376c60b464036a1f477d5c
EmilyPlatzer <emilyplatzer@gmail.com>
2015-04-23 17:01:29 -0600

    Chapter 4, exercise 2 - Reversing an Array

---
+++ chapter4/2reversing_an_array.js
@@ -0,0 +1,42 @@
+function reverseArray(array) {
+  newArray = [];
+  for (i = 0; i < array.length; i ++) {
+    newArray.unshift(array[i]);
+  };
+  return newArray;
+}
+
+function reverseArrayInPlace(array) {
+  var tmp;
+  for (i = 0; i < array.length/2; i ++ ) {
+    tmp = array.slice(i, i+1)[0];
+    array[i] = array[array.length - i - 1];
+    array[array.length - i - 1] = tmp;
+  };
+  return array;
+}
+
+
+console.log("reverseArray")
+var array = [1,2,3,4,5];
+var actual = reverseArray(array);
+console.log("actual:");
+console.log(actual);
+console.log(array);
+
+var expected = [5,4,3,2,1];
+console.log("expected:");
+console.log(expected);
+console.log([1,2,3,4,5]);
+
+console.log("reverseArrayInPlace")
+var array = [1,2,3,4,5];
+var actual = reverseArrayInPlace(array);
+console.log("actual:");
+console.log(actual);
+console.log(array);
+
+var expected = [5,4,3,2,1];
+console.log("expected:");
+console.log(expected);
+console.log(expected);
