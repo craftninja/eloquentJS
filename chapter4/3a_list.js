1dd21871e0986c206316cbbbd535b7dd374237e7
EmilyPlatzer <emilyplatzer@gmail.com>
2015-04-23 18:21:21 -0600

    Chapter 4, exercise 3 - A List, list to array

--- chapter4/3a_list.js
+++ chapter4/3a_list.js
@@ -2,15 +2,32 @@ var arrayToList = function (arr) {
   var node = null
   for (var i = arr.length - 1; i >=0; i -= 1) {
     node = {
-      data: arr[i],
+      value: arr[i],
       rest: node
     };
   };
   return node;
 }
 
+var listToArray = function (list) {
+  var array = [];
+  while (list.rest != null) {
+    array.push(list.value);
+    list = list.rest
+  };
+  array.push(list.value)
+  return array
+}
+
 var actual = arrayToList([1,2,3])
-var expected = "{ data: 1, rest: { data: 2, rest: { data: 3, rest: null } } }"
+var expected = "{ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }"
+console.log('actual:')
+console.log(actual)
+console.log('expected:')
+console.log(expected)
+
+actual = listToArray({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } })
+expected = [1,2,3]
 console.log('actual:')
 console.log(actual)
 console.log('expected:')
