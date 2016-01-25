0fde4e31867a09d976f4d5ce404aa0b850a8cb37
EmilyPlatzer <emilyplatzer@gmail.com>
2016-01-25 15:28:26 -0700

    Revisit a list

--- chapter4/3a_list.js
+++ chapter4/3a_list.js
@@ -1,3 +1,4 @@
+/*
 var arrayToList = function (arr) {
   var list = null;
   for (var i = arr.length - 1; i >=0; i -= 1) {
@@ -54,3 +55,53 @@ actual = nth({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }, 5
 expected = undefined;
 console.log('actual:  ', actual);
 console.log('expected:', expected);
+*/
+// +---------------------------------
+
+// Your code here.
+
+function listToArray (list) {
+  var array = [];
+  var element;
+  for (var i = 0; element = nth(list, i); i ++) {
+    array.push(element);
+  };
+  return array;
+};
+
+function nth (list, number) {
+  if (!list) {
+    return undefined;
+  } else if (number === 0) {
+    return list.value;
+  } else {
+    return nth (list.rest, number - 1);
+  };
+};
+
+function arrayToList (array) {
+  var list = null;
+  array.reverse().forEach(function(element) {
+    list = {
+      value: element,
+      rest: list
+    };
+  });
+  return list;
+};
+
+function prepend (element, list) {
+  return {
+    value: element,
+    rest: list
+  };
+};
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
