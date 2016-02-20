562f8b76c591fa592bb55108e5ad2f88b48d34e1
Aubrey Howell <simplyaubs@gmail.com>
2016-02-20 10:29:39 -0700

    Chapter 5, exercise 4 - some
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

--- chapter5/4every_and_then_some.js
+++ chapter5/4every_and_then_some.js
@@ -6,11 +6,21 @@ function every(list, matcher) {
   return matchy;
 };
 
+function some(list, matcher) {
+  var match = false
+  list.forEach(function(element) {
+    if (matcher(element)) {
+      return match = true;
+    }
+  });
+  return match;
+};
+
 console.log("actual:   ", every([NaN, NaN, NaN], isNaN));
 console.log("expected: ", true);
 console.log('actual:   ', every([NaN, NaN, 4], isNaN));
 console.log('expected: ', false);
-// console.log(some([NaN, 3, 4], isNaN));
-// → true
-// console.log(some([2, 3, 4], isNaN));
-// → false
+console.log('actual:   ', some([NaN, 3, 4], isNaN));
+console.log('expected: ', true);
+console.log('actual:   ', some([2, 3, 4], isNaN));
+console.log('expected: ', false);
