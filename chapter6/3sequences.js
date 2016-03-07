170ef871ef7282156fb746d34af32f55b8dddb31
Emily Platzer <emilyplatzer@gmail.com>
2016-03-07 14:33:06 -0700

    Chapter 6, exercise 3 - sequence interfaces
    Not really sure this is where they wanted to go, buuuut... passes requirements anyway

---
+++ chapter6/3sequences.js
@@ -0,0 +1,36 @@
+// Design an interface that abstracts iteration over a collection of values. An object that provides this interface represents a sequence, and the interface must somehow make it possible for code that uses such an object to iterate over the sequence, looking at the element values it is made up of and having some way to find out when the end of the sequence is reached.
+
+// When you have specified your interface, try to write a function logFive that takes a sequence object and calls console.log on its first five elements—or fewer, if the sequence has fewer than five elements.
+
+// Then implement an object type ArraySeq that wraps an array and allows iteration over the array using the interface you designed. Implement another object type RangeSeq that iterates over a range of integers (taking from and to arguments to its constructor) instead.
+
+function logFive(sequence) {
+  for (var i = 0; i < 5; i++) {
+    if (sequence[i]) {
+      console.log(sequence[i])
+    } else if (sequence.array[i]) {
+      console.log(sequence.array[i])
+    }
+  }
+}
+
+function ArraySeq(array) {
+  this.array = array
+}
+
+function RangeSeq(start, end) {
+  this.array = []
+  for (var i = start; i <= end; i++) {
+    this.array.push(i)
+  }
+}
+logFive([1,2,3,4,5])
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
